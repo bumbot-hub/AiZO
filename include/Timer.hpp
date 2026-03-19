314946fc1ddffbc74441296eef4f6b4767f62050
bumbot-hub <kkamil.kula@gmail.com>
2026-03-19 16:50:19 +0100

    chore: Timer class implemented

--- include/Timer.hpp
+++ include/Timer.hpp
@@ -1,6 +1,22 @@
 #pragma once
+#include<chrono>
 
 class Timer {
+    std::chrono::high_resolution_clock::time_point startTime;
+    bool isRunning = false;
+public:
+    void Start() {
+        startTime = std::chrono::high_resolution_clock::now();
+        isRunning = true;
+    }
+    double ElapsedTimeMs() {
+        if (!isRunning) {
+            return 0.0;
+        }
 
+        auto endTime = std::chrono::high_resolution_clock::now();
+        isRunning = false;
+        return std::chrono::duration<double, std::milli>(endTime - startTime).count();
+    }
 };
 
